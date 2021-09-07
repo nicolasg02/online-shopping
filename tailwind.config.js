2877fad68716f2a9a081c2400c775745273bda16
nicolasg02 <68668870+nicolasg02@users.noreply.github.com>
2021-09-06 21:58:38 -0300

    Project setup

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  // Production
+  // purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
